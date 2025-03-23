45af0776d09d81a6515eeab130f3781a4b6c6bd4
subhash131 <subhashnayak131@gmail.com>
2025-03-23 09:42:23 +0530

    fix prod connection bug

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,23 +1,42 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is not set");
+}
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
 
 export async function connectToDb() {
+  if (cached.conn) {
+    console.log("Using existing database connection");
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    console.log("Creating new database connection");
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        bufferCommands: false, // Improves performance
+      })
+      .then((mongoose) => {
+        return mongoose;
+      })
+      .catch((err) => {
+        console.error("Database connection failed", err);
+        throw err;
+      });
+  }
+
   try {
-    if (isConnected) {
-      console.log("Already connected to database");
-      return mongoose.connection;
-    }
-    const uri = process.env.MONGODB_URI;
-
-    if (!uri) {
-      throw new Error("MONGODB_URI is not set");
-    }
-
-    const db = await mongoose.connect(uri);
-    isConnected = true;
-    return db.connection;
+    cached.conn = await cached.promise;
+    return cached.conn;
   } catch (error) {
-    console.log(error);
+    throw new Error("Failed to connect to database");
   }
 }
