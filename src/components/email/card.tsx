4f0b3b5b2590af583931a92dd66f19f57e128197
subhash131 <subhashnayak131@gmail.com>
2025-03-25 12:03:42 +0530

    add email card

--- src/components/email/card.tsx
+++ src/components/email/card.tsx
@@ -1,7 +1,14 @@
 import { EmailData } from "@/action/gmail/fetch-mails";
+import { formatDateTime } from "@/utils/format-date";
 import React from "react";
 
-const Card = ({ content, sender, subject, timestamp }: EmailData) => {
+const Card = ({
+  content,
+  sender,
+  subject,
+  timestamp,
+  attachments,
+}: EmailData) => {
   return (
     <div className="flex flex-col w-full h-fit p-2 dark:hover:bg-[#353739] hover:bg-gray-300 rounded-md gap-1 text-sm">
       <div className="flex w-full gap-2">
@@ -9,14 +16,20 @@ const Card = ({ content, sender, subject, timestamp }: EmailData) => {
           SD
         </div>
         <div className="">
-          <p className="">{sender}</p>
+          <p className="">
+            {sender} {formatDateTime(timestamp)}
+          </p>
           <p className="text-xs">{subject}</p>
         </div>
       </div>
       <div className="size-full">
-        <p className="text-wrap">{content}</p>
+        <p className="whitespace-normal break-words">{content}</p>
       </div>
-      <div>attachments</div>
+      <div>
+        {attachments.map(({ filename, attachmentId }) => {
+          return <div key={attachmentId}>{filename}</div>;
+        })}
+      </div>
     </div>
   );
 };
